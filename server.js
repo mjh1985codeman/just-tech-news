9224da36250496c6c89bcba8ceb6a7c7df6d5ad4
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-09-01 12:58:49 -0500

    completed post routes

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 // turn on routes
 app.use(routes);
 
-// turn on connection to db and server
+// turn on connection to database and server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
 });
